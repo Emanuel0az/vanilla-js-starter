4fbf402dd0bbd4d8d68939d5c44b562835de3b25
Emanuel0az <emanuelabarcazuniga@gmail.com>
2024-06-03 15:24:02 -0600

    inicio de funcion async de la API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 // const { text } = require("express")
 
 // Inserte el código aquí
-let texto = document.getElementById('texto')
-let boton = document.getElementById('addTarea')
+let input = document.getElementById('texto')
+let boton = document.getElementById ('addTarea')
+let div = document.getElementById ('vacioCont')
+// let boton = document.getElementById('addTarea')
 
 
 boton.addEventListener('click', function (){
-    let nuevoDiv = document.createElement('div')
-    let nuevoText = texto
-    nuevoDiv.appendChild(nuevoText)
-
+    
+    let task = document.createElement('div')
+    task.style.border= "1px solid black";
+    task.style.padding= "3px"
+    task.style.gap = "3px"
+    task.innerHTML = input.value
+    div.appendChild(task)
 })
+
+async function addTask() {
+    try { //si promesa se resuelve, https status 200
+      const response = await fetch('http://localhost:3000/api/task');
+      const data = await response.json(); //Await pausa la ejecución de una función hasta que la promisa sea resuelva
+      console.log(data);
+      //capturar pokemon
+    } catch (error) { //si promesa no se resuelve, https status 400-499 y 500-599
+      console.error(error);
+    }
+  }
