6433f7f405a99351b480fbafc2e928921e287e5f
Emanuel0az <emanuelabarcazuniga@gmail.com>
2024-06-03 10:11:43 -0600

    creacion de estilos de la caja de tareas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// Inserte el código aquí
+// const { text } = require("express")
 
+// Inserte el código aquí
+let texto = document.getElementById('texto')
+let boton = document.getElementById('addTarea')
+
+
+boton.addEventListener('click', function (){
+    let texto = document.createElement('div')
+    
+})
